c750f6240da4b1f27f088d66376aee248a58d6e8
gitmaxlla <user@email>
2025-10-29 22:32:42 +0300

    initial commit

---
+++ webpartytime/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { motion } from 'motion-v'
+
+
+defineProps<{ msg: string }>()
+
+const dragConstraints = ref(null)
+const count = ref(0)
+</script>
+
+<template>
+  <div ref="dragConstraints" style="width: 500px; height: 300px;">
+    <motion.h1
+      drag
+      :drag-constraints="dragConstraints"
+      :drag-elastic="0.5"
+      :initial="{ opacity: 0 }" :animate="{ opacity: 1, transition: { duration: 0.3 } }"
+      :whileHover="{ scale: 1.5 }">
+        {{ msg }}
+    </motion.h1>
+  </div>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
