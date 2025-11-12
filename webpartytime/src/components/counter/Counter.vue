650c9c99a65fb5ce335b638a5d2a6b61b487c2b7
gitmaxlla <user@email>
2025-11-13 00:08:23 +0300

    proof of concept guess the number game added

--- webpartytime/src/components/counter/Counter.vue
+++ webpartytime/src/components/counter/Counter.vue
@@ -1,11 +1,36 @@
 <template>
   <div>
+    <p>Угадай число от 0 до 10</p>
     <p>Online: {{ count }}</p>
+
+    <br/>
+
+    <input type="button" @click="onMakeRoomButtonClicked" value="Создать комнату" />
+    <label>Номер комнаты: {{ roomId }}</label>
+    
+    <br/>
+
+    <input type="text" v-model="roomIdInput"></input>
+    <input type="button" @click="onJoinRoomButtonClicked" value="Присоединиться к комнате" />
+
+    <br/>
+
+    <input type="text" v-model="msg"></input>
+    <input type="button" @click="onSendMessageButtonClicked" value="Отправить сообщение" />
+
+    <br/>
+
+    <p>Чат</p>
+    <p v-for="(item, index) in items" :key="index">{{ item }}</p>
+
+    <br/>
+
+    <p>{{ serverMsg }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, type Ref, ref } from 'vue'
 import { Centrifuge } from 'centrifuge';
 
 export default defineComponent({
@@ -14,8 +39,14 @@ export default defineComponent({
   data() {
     return {
       count: 0 as number,
+      roomId: "#" as string,
+      serverMsg: "" as string,
+      roomIdInput: ref('') as Ref<string>,
+      msg: ref('') as Ref<string>,
+      items: [] as any,
       centrifuge: null as Centrifuge | null,
-      onlineCountSub: null as any
+      onlineCountSub: null as any,
+      gameRoomSub: null as any,
     }
   },
 
@@ -36,6 +67,10 @@ export default defineComponent({
         this.count = Object.keys(data['clients']).length
       })
 
+      this.centrifuge.on('message', (context) => {
+        this.serverMsg = context.data
+      })
+
       this.centrifuge.connect()
     },
 
@@ -46,6 +81,36 @@ export default defineComponent({
       if (this.centrifuge) {
         this.centrifuge.disconnect()
       }
+    },
+
+    onMakeRoomButtonClicked(event: MouseEvent): void {
+      this.roomId = String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+      this.gameRoomSub = this.centrifuge?.newSubscription(this.roomId)
+      this.gameRoomSub.subscribe()
+      this.setPublicationCallback()
+    },
+
+    onJoinRoomButtonClicked(event: MouseEvent): void {
+      this.gameRoomSub = this.centrifuge?.newSubscription(this.roomIdInput)
+      this.gameRoomSub.subscribe()
+      this.roomId = this.roomIdInput
+      this.setPublicationCallback()
+    },
+
+    setPublicationCallback() {
+      this.gameRoomSub.on('publication', (context: any) => {
+        console.log(context.data)
+        var displayedMessage = ""
+        if (context.data["Presenter"]) {
+          displayedMessage += "ведущий -> "
+        }
+        displayedMessage += context.data["Message"] 
+        this.items.push(displayedMessage)
+      })
+    },
+
+    onSendMessageButtonClicked(event: MouseEvent): void {
+      this.centrifuge?.publish(this.roomId, {"Message": this.msg, "Presenter": false})
     }
   },
 
@@ -55,6 +120,6 @@ export default defineComponent({
 
   beforeUnmount(): void {
     this.disconnectCentrifuge()
-  }
+  },
 })
 </script>
